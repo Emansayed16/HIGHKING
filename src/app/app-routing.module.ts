d4de2e222848d8976209f9554d7e561ce8e5429b
Emansayed16 <eman34220@gmail.com>
2022-05-16 23:44:05 +0200

    HIGHKING uploading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogsComponent } from './blogs/blogs.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HomeComponent } from './home/home.component';
+import { LocationComponent } from './location/location.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { TestemonialsComponent } from './testemonials/testemonials.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'location', component:LocationComponent},
+  {path:'contacts', component:ContactsComponent},
+  {path:'navbar', component:NavbarComponent},
+  {path:'blogs', component:BlogsComponent},
+  {path:'testemonials', component:TestemonialsComponent},
+  {path:'**',component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LocationComponent } from './location/location.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { TestemonialsComponent } from './testemonials/testemonials.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LocationComponent,
+    BlogsComponent,
+    TestemonialsComponent,
+    ContactsComponent,
+    HomeComponent,
+    NotfoundComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
